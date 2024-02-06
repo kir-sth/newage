6bf8a3c3472fed312f7d423d44f74e70fcd23f0b
kir-sth <kir.sth@yandex.ru>
2024-02-06 22:39:41 +0300

    config validation adding

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-import config
+from config_reader import config
 from handlers import router
 
 
 async def main():
-    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
