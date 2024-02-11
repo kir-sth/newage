14fdd70c5c0e5f221455247d5fba40f4c5d9dd2a
kir-sth <kir.sth@yandex.ru>
2024-02-11 17:01:11 +0300

    refactoring

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from aiogram import types, Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -7,10 +8,19 @@ from utils import Admin
 
 router = Router()
 admin = Admin()
+=======
+from aiogram import types, F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+
+router = Router()
+>>>>>>> 16333f6 (reverse)
 
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
+<<<<<<< HEAD
     await msg.answer("Привет!")
     admin.set(msg.chat.id)
 
@@ -21,3 +31,11 @@ async def news_handler(msg: types.Message):
 
 
 
+=======
+    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    await msg.answer(f"Твой ID: {msg.from_user.id}")
+>>>>>>> 16333f6 (reverse)
